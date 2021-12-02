b875a6169981348f24a9e34251420c08ccb3f077
user1 <user1@DESKTOP-5U51QT0>
2021-12-02 22:11:43 +0900

    12-02 아무거나

--- ComunityWeb_SemiProject/src/main/java/com/web/board/model/BoardDTO.java
+++ ComunityWeb_SemiProject/src/main/java/com/web/board/model/BoardDTO.java
@@ -13,7 +13,7 @@ public class BoardDTO {
 	
 	public BoardDTO() {}
 	public BoardDTO(int board_num) {
-		this.board_num = board_num;
+		this.board_num = board_num;///
 	}
 	
 	public int getPost_num() {
