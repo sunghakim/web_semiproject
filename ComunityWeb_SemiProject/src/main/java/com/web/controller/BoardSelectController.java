a5a72d2996f413c972d336f57748b54c780f1e6e
user1 <user1@DESKTOP-5U51QT0>
2021-12-07 13:01:17 +0900

    메인페이지(전체글 불러오기), 선택한 게시판의 게시글 불러오기 컨트롤러 수정

--- ComunityWeb_SemiProject/src/main/java/com/web/controller/BoardSelectController.java
+++ ComunityWeb_SemiProject/src/main/java/com/web/controller/BoardSelectController.java
@@ -3,6 +3,7 @@ package com.web.controller;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,21 +20,22 @@ public class BoardSelectController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		Integer board_select = Integer.parseInt(request.getParameter("board_num")); //보드 번호 받아오기
+		Integer page_num = Integer.parseInt(request.getParameter("page_num")); //페이지수 받아오기
+		BoardService service = new BoardService();
+		List<BoardDTO> datas = service.searchBoard(board_select, page_num);
 		
-		
+		request.setAttribute("datas", datas); //리스트 datas를 리퀘스트 객체에 셋
+		String view = "WEB-INF/jsp/board/board.jsp"; //포워드 페이지(
+		RequestDispatcher rd = request.getRequestDispatcher(view);
+		rd.forward(request, response);
 	}
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Integer board_select = Integer.parseInt(request.getParameter("board_num")); //보드 번호 받아오기
-		Integer page_num = Integer.parseInt(request.getParameter("page_num")); //페이지수 받아오기
 		
-		BoardService service = new BoardService();
-		if(board_select != null) {
-			System.out.println("선택한 카테고리: " + board_select);
-			List<BoardDTO> datas = service.searchBoard(board_select, page_num);//서비스에서 선택한 게시판의 글 불러오기
-		}
 	}
 
 }
--- ComunityWeb_SemiProject/src/main/java/com/web/controller/MainpageController.java
+++ ComunityWeb_SemiProject/src/main/java/com/web/controller/MainpageController.java
@@ -21,15 +21,19 @@ public class MainpageController extends HttpServlet {
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Integer page_num = Integer.parseInt(request.getParameter("page_num")); //페이지수 받아오기
+		MainpageService service = new MainpageService();//서비스 불러오기
+		List<MainpageDTO> datas = service.searchPage(page_num); //전체 카테고리 게시글 10개 받아오기
 		
+		request.setAttribute("datas", datas); //리스트 datas를 리퀘스트 객체에 셋
+		String view = "WEB-INF/jsp/board/mainpage.jsp"; //포워드 페이지
+		RequestDispatcher rd = request.getRequestDispatcher(view);
+		rd.forward(request, response);
 	}
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int page_num = Integer.parseInt(request.getParameter("page_num")); //페이지수 받아오기
 		
-		MainpageService service = new MainpageService(); //서비스 불러오기
-		List<MainpageDTO> datas = service.searchPage(page_num); //전체 게시글 불러오기(페이지수 보고)
 	}
 
 }
