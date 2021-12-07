ef3bf54c1c843b521b2cc3654c04024bbaddf20a
user1 <user1@DESKTOP-5U51QT0>
2021-12-07 19:19:52 +0900

    게시판 dao 넣어놨음.

--- ComunityWeb_SemiProject/src/main/java/com/web/controller/BoardSelectController.java
+++ ComunityWeb_SemiProject/src/main/java/com/web/controller/BoardSelectController.java
@@ -26,17 +26,18 @@ public class BoardSelectController extends HttpServlet {
 			throws ServletException, IOException {
 		Integer board_select = Integer.parseInt(request.getParameter("board_select")); //보드 번호 받아오기
 		Integer page_num = Integer.parseInt(request.getParameter("page_num")); //페이지수 받아오기
-		BoardService service = new BoardService();
-		List<BoardDTO> datas = service.searchBoard(board_select, page_num);
+		BoardService service = new BoardService(); //보드 서비스 불러오기
+		List<BoardDTO> datas = service.searchBoard(board_select, page_num); //게시판넘버랑 게시글 번호 매개변수 받아서 게시글 불러오기 
+		request.setAttribute("datas", datas); //게시글 리스트를 셋
 		
 		BoardManageDAO category = new BoardManageDAO(); //카테고리 받아오는것
-		List<BoardManageDTO> cate_list = category.boardList(); //list 컬렉션으로 board_num, board_name 넣기
-		request.setAttribute("cate_list", cate_list); //메인.jsp 카테고리에 게시판 리스트 가져오기
+		List<BoardManageDTO> cate_list = category.boardList(); //list 컬렉션으로 board_num, board_name 받아오기
+		request.setAttribute("cate_list", cate_list); //메인.jsp 카테고리에 게시판 리스트 셋
 		
-		request.setAttribute("datas", datas); //리스트 datas를 리퀘스트 객체에 셋
+		
 		String view = "WEB-INF/jsp/board/boardList.jsp"; //포워드 페이지(
 		RequestDispatcher rd = request.getRequestDispatcher(view);
-		rd.forward(request, response);
+		rd.forward(request, response); //포워드
 	}
 
 	
